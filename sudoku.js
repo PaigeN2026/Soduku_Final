b0a361712dd950dbd173402704c435dfed8b3768
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-05-30 22:44:35 +0000

    5/30

--- sudoku.js
+++ sudoku.js
@@ -13,18 +13,18 @@ console.log("hello world");
 var numSelected = null;
 var tileSleected = null;
 
-var errors = 0;
+var error = 0;
 
 var board = [
-    "- 2 - - - - - - -",
-    "- - - 6 - -  - - 3",
-    "- 7 4 - 8 - - - -",
-    "- - - - - 3 - - 2",
-    "- 8 - - 4 - - 1 -",
-    "6 - - 5 - - - - -",
-    "- - - - 1 - 7 8 -",
-    "5 - - - - 9 - - -",
-    "- - - - - - - 4 -"
+    "-2-------",
+    "---6----3",
+    "-74-8----",
+    "-----3--2",
+    "-8--4--1-",
+    "6--5-----",
+    "----1-78-",
+    "5----9---",
+    "-------4-"
 ]
 
 var solution = [
@@ -44,7 +44,63 @@ window.onload = function() {
 }
 
 function setGame() {
-    for (let i = 1; i <= 9; 1++) {
+    // digits 1-9
+    for (let i = 1; i <= 9; i++) {
+        // <div id="1" class="number"></div>
         let number = document.createElement("div");
+        number.id = i
+        number.innerText = i;
+        number.addEventListener("click", selectNumber);
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+    // board 9x9
+    for (let r = 0; r < 9; r++){
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+            tile.addEventListener("click", selectTile);
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+
+function selectNumber(){
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
+
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
+
+        let coords =  this.id.split("-"); // ["0", "0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        }
+        else {
+            error += 1;
+            document.getElementById("error").innerText = error;
+        }
     }
 }
